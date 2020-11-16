d62c46b55b9d4c32574dce2510212e467de686e8
Lucas <lucasion@hotmail.com>
2020-11-16 08:41:03 +0100

    Interface piss

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        interface IDrawable
+        {
+            /// <summary>
+            /// Draws the object
+            /// </summary>
+            void Draw();
+        }
+
+        class Box : IDrawable
+        {
+            public void Draw()
+            {
+                // Unique Procedure to draw a Box
+            }
+        }
+
+        class Circle : IDrawable
+        {
+            public void Draw()
+            {
+                // Unique Procedure to draw a Circle
+            }
+        }
+        class Triangle : IDrawable
+        {
+            public void Draw()
+            {
+                // Unique Procedure to draw a Triangle
+            }
+        }
+    }
+}
